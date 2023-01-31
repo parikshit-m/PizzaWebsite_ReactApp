1c35287a9ec7c42293224d54cf1ceb7a47a09553
PARIKSHIT MITKAR <parikshit.m098@gmail.com>
2023-01-31 20:25:19 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './component/Footer';
 import Menu from './pages/Menu';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import React, { Component }  from 'react';
 
 function App() {
   return (
