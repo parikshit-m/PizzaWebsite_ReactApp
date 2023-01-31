2002024d0bba2e108fbb086b6fff93750968107a
PARIKSHIT MITKAR <parikshit.m098@gmail.com>
2023-01-31 19:43:51 +0530

    Updates

---
+++ precache-manifest.6595e508c1e58e7865a679623a605d33.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e66eed4621869185207c",
+    "url": "/PizzaWebsite_ReactApp/static/css/main.37c8218a.chunk.css"
+  },
+  {
+    "revision": "e66eed4621869185207c",
+    "url": "/PizzaWebsite_ReactApp/static/js/main.e66eed46.chunk.js"
+  },
+  {
+    "revision": "fa677b773aaf13c72ca8",
+    "url": "/PizzaWebsite_ReactApp/static/js/1.fa677b77.chunk.js"
+  },
+  {
+    "revision": "f3b97a006d4955749397",
+    "url": "/PizzaWebsite_ReactApp/static/js/2.f3b97a00.chunk.js"
+  },
+  {
+    "revision": "7db1f83241022a82fcfc",
+    "url": "/PizzaWebsite_ReactApp/static/js/runtime~main.7db1f832.js"
+  },
+  {
+    "revision": "d85cec468342f3dcee087c02bf6dd430",
+    "url": "/PizzaWebsite_ReactApp/static/media/pizzaLogo.d85cec46.png"
+  },
+  {
+    "revision": "4aa5292cac69561b68b10b2db0f3f6c1",
+    "url": "/PizzaWebsite_ReactApp/static/media/pizza.4aa5292c.jpeg"
+  },
+  {
+    "revision": "08e289f85265b3d2b3984db3d424b705",
+    "url": "/PizzaWebsite_ReactApp/static/media/pepperoni.08e289f8.jpg"
+  },
+  {
+    "revision": "434161f0b4c193f9ce5e1061f26dbf40",
+    "url": "/PizzaWebsite_ReactApp/static/media/margherita.434161f0.jpg"
+  },
+  {
+    "revision": "05f53a063fe35296c578593c523ab1a1",
+    "url": "/PizzaWebsite_ReactApp/static/media/pedrotechspecial.05f53a06.jpg"
+  },
+  {
+    "revision": "1672e7ad52375097a95ec88034fbc0cb",
+    "url": "/PizzaWebsite_ReactApp/static/media/vegan.1672e7ad.jpg"
+  },
+  {
+    "revision": "44f59bed20e652447ce9f2f2c9340c65",
+    "url": "/PizzaWebsite_ReactApp/static/media/pineapple.44f59bed.jpg"
+  },
+  {
+    "revision": "7f204ab1930eb1878e1a3326ec315244",
+    "url": "/PizzaWebsite_ReactApp/static/media/expensive.7f204ab1.jpg"
+  },
+  {
+    "revision": "67f4f9031213dbbb6c7aaf1795c57ef8",
+    "url": "/PizzaWebsite_ReactApp/static/media/multiplePizzas.67f4f903.jpeg"
+  },
+  {
+    "revision": "e29e930c1870f3fcc15edb3a8c3ade19",
+    "url": "/PizzaWebsite_ReactApp/static/media/pizzaLeft.e29e930c.jpg"
+  },
+  {
+    "revision": "7a0691fe1aee7bd251b4631af761b59a",
+    "url": "/PizzaWebsite_ReactApp/index.html"
+  }
+];
